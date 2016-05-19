9e409019d135198bce7fc6924574c4d861df4796
Roger Dering <rogerdering@hotmail.com>
2016-05-19 16:55:01 +0200

    completed part 2

--- Usr-App/static/custom.js
+++ Usr-App/static/custom.js
@@ -1,23 +1,22 @@
 $(document).ready(function() {
   $('#ajax').keyup(function() {
-    console.log("searching...")
-    var data = {
-      search: $( '#ajax' ).val()
-    }
-    $.post('/api', data, function(result) {
-      var lastVariableEver = result.filter(function(elem, pos) {
-        return result.indexOf(elem) == pos;
-      }); 
-
-      $('#json-datalist').empty()
-
-      var usersOption = ''
-      for (var m = 0; m < lastVariableEver.length; m++) {
-        usersOption += '<option value="'+lastVariableEver[m]+'" />'
+    setTimeout(function() {
+      var data = {
+        search: $( '#ajax' ).val()
       }
-      $('#json-datalist').append(usersOption)
-    })
+      $.post('/api', data, function(result) {
+        var lastVariableEver = result.filter(function(elem, pos) {
+          return result.indexOf(elem) == pos;
+        }); 
+
+        $('#json-datalist').empty()
+
+        var usersOption = ''
+        for (var m = 0; m < lastVariableEver.length; m++) {
+          usersOption += '<option value="'+lastVariableEver[m]+'" />'
+        }
+        $('#json-datalist').append(usersOption)
+      })
+    }, 300)
   })
 })
-
-
